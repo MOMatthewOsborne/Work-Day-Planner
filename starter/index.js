5929ce87af1ef2e70b2d3fdb1762ec2c63ad9318
Matthew Osborne <momatthewosborne1@gmail.com>
2022-12-12 11:45:54 +0000

    colours now working

--- starter/index.js
+++ starter/index.js
@@ -1,5 +1,8 @@
 var timeList = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 var containerEL = $('#container');
+var currentHour = moment().format("HH");
+console.log(currentHour)
+
 
 $(document).ready(function () {
 
@@ -18,6 +21,30 @@ $(document).ready(function () {
 
 
     // }
+
+    function timeUpdate() {
+        $('.time-block').each(function () {
+            var tableTime = parseInt($(this).children().attr('id'));
+            console.log(tableTime)
+            // currentHour += parseInt(currentHour);
+            if (tableTime < currentHour) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            } else if (tableTime == currentHour) {
+                $(this).removeClass("future");
+                $(this).addClass("present");
+                $(this).removeClass("past");
+            } else {
+                $(this).addClass("future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+            }
+
+        })
+
+    }
+    timeUpdate()
 
 
 })
