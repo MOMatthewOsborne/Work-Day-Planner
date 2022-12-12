d76969efd2e1672e2e799c85b961a8eae21f980e
Matthew Osborne <momatthewosborne1@gmail.com>
2022-12-12 15:18:48 +0000

    button now works. local storage now works

--- starter/index.js
+++ starter/index.js
@@ -2,9 +2,30 @@ var timeList = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"
 var containerEL = $('#container');
 var currentHour = moment().format("HH");
 console.log(currentHour)
+var nine = document.getElementsByClassName("nine");
+var ten = document.getElementsByClassName("ten")
+var eleven = document.getElementsByClassName("eleven");
+var twelve = document.getElementsByClassName("twelve");
+var thirteen = document.getElementsByClassName("thirteen");
+var fourteen = document.getElementsByClassName("fourteen");
+var fifteen = document.getElementsByClassName("fifteen");
+var sixteen = document.getElementsByClassName("sixteen");
+var seventeen = document.getElementsByClassName("seventeen");
+
 
 
 $(document).ready(function () {
+
+    $(".nine").val(window.localStorage.getItem("9am"))
+    $(".ten").val(window.localStorage.getItem("10am"))
+    $(".eleven").val(window.localStorage.getItem("11am"))
+    $(".twelve").val(window.localStorage.getItem("12pm"))
+    $(".thirteen").val(window.localStorage.getItem("1pm"))
+    $(".fourteen").val(window.localStorage.getItem("2pm"))
+    $(".fifteen").val(window.localStorage.getItem("3pm"))
+    $(".sixteen").val(window.localStorage.getItem("4pm"))
+    $(".seventeen").val(window.localStorage.getItem("5pm"))
+    console.log(nine)
 
     // This function shows the correct date at the top of the page
     function showToday() {
@@ -44,7 +65,14 @@ $(document).ready(function () {
         })
 
     }
-    timeUpdate()
+
+    timeUpdate();
+
+    $(".saveBtn").on("click", function () {
+        var noteWritten = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, noteWritten)
+    })
 
 
 })
